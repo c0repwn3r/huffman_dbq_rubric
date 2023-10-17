d747ca7258ec938d7826cda3e8ce32fdc5a822f0
core <core@coredoes.dev>
2023-10-17 13:35:34 -0400

    new rubric pt2 + test score calculator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,10 @@
     let s = 0;
     let e = 0;
 
+    let huff_rubric = {d: 3, s: 6, e: 3};
+    let ap_rubric = {d: 3, s: 4, e: 2};
+    let rubric = huff_rubric;
+
     function updateDSE() {
         describes = false;
         supports = false;
@@ -34,9 +38,9 @@
             if (doc.supports) s += 1;
             if (doc.explains) e += 1;
         }
-        if (d >= 3) describes = true;
-        if (s >= 6) supports = true;
-        if (e >= 3) explains = true;
+        if (d >= rubric.d) describes = true;
+        if (s >= rubric.s) supports = true;
+        if (e >= rubric.e) explains = true;
         console.log(documents);
     }
 
@@ -67,6 +71,51 @@
     function reset() {
         window.location.reload();
     }
+
+    let new_or_old = "Huff's Rubric";
+    let new_rubric = false;
+
+    function updateRubrics() {
+        if (new_rubric) {
+            new_or_old = "CollegeBoard Rubric (New Rubric)";
+            rubric = ap_rubric;
+        } else {
+            new_or_old = "Huff's Rubric";
+            rubric = huff_rubric;
+        }
+        updateDSE();
+        updateScore();
+        complexity = false; // reset complexity scores because the rubric entirely changes here
+        new_rubric_complexity_criteria = new_rubric_complexity_criteria_base; // see above
+    }
+
+    let new_rubric_complexity_criteria_base = [
+        {crit:"Exploring nuance by analyzing multiple themes or perspectives",ach:false},
+        {crit:"Explaining both similarity and difference, both continuity and change, both causes and effects, multiple causes or effects, multiple similarities or differences, or multiple continuities or changes",ach:false},
+        {crit:"Explaining relevant and insightful connections within and across periods or geographical areas",ach:false},
+        {crit:"Effectively uses ALL SEVEN documents to support an argument",ach:false},
+        {crit:"Effective HAPPY analysis for at least FOUR documents",ach:false},
+        {crit:"Uses documents and outside evidence to demonstrate sophisticated understanding",ach:false}
+    ];
+    let new_rubric_complexity_criteria = new_rubric_complexity_criteria_base;
+
+    function updateNewRubricComplexity() {
+        complexity = false;
+        for (let i = 0; i < new_rubric_complexity_criteria.length; i++) {
+            if (new_rubric_complexity_criteria[i].ach) {
+                complexity = true;
+            }
+        }
+        updateDSE();
+        updateScore();
+    }
+
+    $: new_rubric_complexity_criteria, updateNewRubricComplexity();
+
+    $: new_rubric, updateRubrics();
+
+    let mcq = 50;
+    let curve = 0;
 </script>
 
 <div class="flex justify-center h-screen space-y-5">
@@ -74,7 +123,14 @@
         <h1 class="text-3xl font-bold">
             Huffman History DBQ Rubric Checklist!
         </h1>
-        <p>Checklist with the DBQ rubric so I can distract myself from actually self-grading my DBQ.</p>
+        <h3 class="text-xl font-bold">
+            (and test score calculator)
+        </h3>
+        <p>Checklist with the DBQ rubrics so you can grade yourself on DBQs you write. Supports both the old rubric (Huff's rubric) and the new rubric (used for AP Exams). Also includes a test score calculator so you can figure out what your total test score will be.</p>
+
+        <i>To use the new rubric (the one for the AP exam), scroll to the bottom of the page</i>
+
+        <i>Version 1.2.0 - by Tyler/@coredoesdev</i>
 
         <hr>
 
@@ -123,34 +179,36 @@
 
             {#each [0, 1, 2, 3, 4, 5, 6] as val}
                 <tr>
-                    <td><input type="number" class="input w-full" bind:value={documents[val].num} /></td>
-                    <td><input type="checkbox" class="checkbox" class:checkbox-error={d < 3} class:checkbox-success={d >= 3} bind:checked={documents[val].describes} /></td>
-                    <td><input type="checkbox" class="checkbox" class:checkbox-error={s < 6} class:checkbox-success={s >= 6} bind:checked={documents[val].supports} /></td>
-                    <td><input type="checkbox" class="checkbox" class:checkbox-error={e < 3} class:checkbox-success={e >= 3} bind:checked={documents[val].explains} /></td>
+                    <td><input type="number" class="input w-full p-2" bind:value={documents[val].num} /></td>
+                    <td><input type="checkbox" class="checkbox" class:checkbox-error={!describes} class:checkbox-success={describes} bind:checked={documents[val].describes} /></td>
+                    <td><input type="checkbox" class="checkbox" class:checkbox-error={!supports} class:checkbox-success={supports} bind:checked={documents[val].supports} /></td>
+                    <td><input type="checkbox" class="checkbox" class:checkbox-error={!explains} class:checkbox-success={explains} bind:checked={documents[val].explains} /></td>
                 </tr>
             {/each}
 
             <tr>
-                <td></td>
+                <td>
+                    <i>Rubric: {new_or_old}</i>
+                </td>
                 <td>
                     {#if describes}
-                        <p class="text-success">{d}/3</p>
+                        <p class="text-success">{d}/{rubric.d}</p>
                     {:else}
-                        <p class="text-error">{d}/3</p>
+                        <p class="text-error">{d}/{rubric.d}</p>
                     {/if}
                 </td>
                 <td>
                     {#if supports}
-                        <p class="text-success">{s}/6</p>
+                        <p class="text-success">{s}/{rubric.s}</p>
                     {:else}
-                        <p class="text-error">{s}/6</p>
+                        <p class="text-error">{s}/{rubric.s}</p>
                     {/if}
                 </td>
                 <td>
                     {#if explains}
-                        <p class="text-success">{e}/3</p>
+                        <p class="text-success">{e}/{rubric.e}</p>
                     {:else}
-                        <p class="text-error">{e}/3</p>
+                        <p class="text-error">{e}/{rubric.e}</p>
                     {/if}
                 </td>
             </tr>
@@ -177,21 +235,44 @@
         <h2 class="text-xl font-bold">
             Complexity
         </h2>
-        <b>Demonstrates a complex understanding of the historical development that is the focus of the prompt, using evidence to corroborate, qualify, or modify an argument that addresses the question.</b>
-        <p>The response must demonstrate a complex understanding, which must be part of the argument and not merely a phrase or reference. This <i>could</i> include:</p>
-        <ul class="list-disc">
-            <li class="list-item">Explaining nuance by analyzing multiple variables</li>
-            <li class="list-item">Explaining both similarity and difference, both continuity and change, or multiple causes, or both causes and effects</li>
-            <li class="list-item">Explaining relevant and insightful connections within and across periods</li>
-            <li class="list-item">Confirming the validity of an argument by corroborating multiple perspectives across themes</li>
-            <li class="list-item">Qualifying or modifying an argument by considering diverse or alternate views or evidence</li>
-        </ul>
-        <div class="form-control text-center">
-            <label class="label cursor-pointer text-right justify-end">
-                <span class="label-text float-right mr-3">Does the DBQ meet this criteria?</span>
-                <input class:checkbox-error={!complexity} class:checkbox-success={complexity} type="checkbox" bind:checked={complexity} class="checkbox float-left" />
-            </label>
-        </div>
+
+        {#if new_rubric}
+            <b>Demonstrates a complex understanding of the historical development that is the focus of the prompt through sophisticated argumentation and/or effective use of evidence.</b>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>Criteria</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    {#each [0, 1, 2, 3, 4, 5] as val}
+                        <tr>
+                            <td>{new_rubric_complexity_criteria[val].crit}</td>
+                            <td><input type="checkbox" class="checkbox" class:checkbox-error={!complexity} class:checkbox-success={complexity} bind:checked={new_rubric_complexity_criteria[val].ach} /></td>
+                        </tr>
+                    {/each}
+                </tr>
+                </tbody>
+            </table>
+        {:else}
+            <b>Demonstrates a complex understanding of the historical development that is the focus of the prompt, using evidence to corroborate, qualify, or modify an argument that addresses the question.</b>
+            <p>The response must demonstrate a complex understanding, which must be part of the argument and not merely a phrase or reference. This <i>could</i> include:</p>
+            <ul class="list-disc">
+                <li class="list-item">Explaining nuance by analyzing multiple variables</li>
+                <li class="list-item">Explaining both similarity and difference, both continuity and change, or multiple causes, or both causes and effects</li>
+                <li class="list-item">Explaining relevant and insightful connections within and across periods</li>
+                <li class="list-item">Confirming the validity of an argument by corroborating multiple perspectives across themes</li>
+                <li class="list-item">Qualifying or modifying an argument by considering diverse or alternate views or evidence</li>
+            </ul>
+            <div class="form-control text-center">
+                <label class="label cursor-pointer text-right justify-end">
+                    <span class="label-text float-right mr-3">Does the DBQ meet this criteria?</span>
+                    <input class:checkbox-error={!complexity} class:checkbox-success={complexity} type="checkbox" bind:checked={complexity} class="checkbox float-left" />
+                </label>
+            </div>
+        {/if}
 
         <hr>
 
@@ -200,9 +281,33 @@
         </h2>
 
         <p>Points: {points}/7</p>
-        <p>Grade: {grade}/50</p>
+        <p>Grade: {grade}/50 ({(grade/50)*100}%)</p>
+
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">MCQ score (/50)</span>
+            </label>
+            <input type="number" bind:value={mcq} placeholder="45" class="input input-bordered w-full max-w-xs" />
+        </div>
+
+        <div class="form-control w-full max-w-xs">
+            <label class="label">
+                <span class="label-text">Curve (points)</span>
+            </label>
+            <input type="number" bind:value={curve} placeholder="0" class="input input-bordered w-full max-w-xs" />
+        </div>
+
+        <p>Test Grade (including MCQ): {grade + mcq}/100</p>
+        <p>Test Grade (MCQ+Curve): {grade+mcq+curve}/100</p>
 
         <button on:click={reset} class="btn btn-primary">Reset</button>
+
+        <div class="justify-center form-control w-1/3">
+            <label class="label cursor-pointer">
+                <span class="label-text">{new_or_old}</span>
+                <input type="checkbox" class="toggle" bind:checked={new_rubric} />
+            </label>
+        </div>
     </div>
 </div>
 
